38ecb64ee877951fb77ac2a289f6c7b78a9cbd63
neymarjimoh <jemohkunle2007@gmail.com>
2022-01-23 08:57:32 +0100

    modified sum to a variadic function

--- calc/calc.go
+++ calc/calc.go
@@ -1,7 +1,13 @@
 package calc
 
-func Add(a, b int) int {
-	return a + b
+func Add(args  ...int) int {
+	sum := 0
+
+	for _, val := range args {
+		sum += val
+	}
+
+	return sum
 }
 
 func Sub(a, b int) int {
